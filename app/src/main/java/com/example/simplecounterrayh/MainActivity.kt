5fb660c6f47c17c96bfbc5e09dca8561eb282413
rahub1 <Rayhubbard72001@gmail.com>
2022-09-07 11:06:41 -0400

    Initial Commit.

---
+++ app/src/main/java/com/example/simplecounterrayh/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.simplecounterrayh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity() : AppCompatActivity(), Parcelable {
+    var counter = 0
+
+    constructor(parcel: Parcel) : this() {
+        counter = parcel.readInt()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val upgradeButton = findViewById<Button>(R.id.upgradeBtn)
+        button.setOnClickListener {
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 100) {
+
+                // Show upgrade button and set onClickListener
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 2"
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(counter)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
